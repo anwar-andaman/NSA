022cd660050e3872ec3fa3d90f4ebf0758b8036d
anwar-andaman <116252042+anwar-andaman@users.noreply.github.com>
2023-07-06 10:24:30 +0530

    Add files via upload

---
+++ 12-copy.sh
@@ -0,0 +1,18 @@
+#Program to copy a set of files from given as pair at command prompt
+if [ `expr $# % 2` -ne 0 -o $# -eq 0 ]
+then
+	echo "syntax error"
+	echo "<$0> <source file> <destination file> etc..."
+	echo "Arguments must be even number"
+	exit 1
+fi
+i=`expr $# / 2`
+while [ $i -gt 0 ]
+do
+cp $1 $2
+shift
+shift
+i=`expr $i - 1`
+done
+exit 0
+
